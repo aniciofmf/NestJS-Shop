f1176f9564672636c0fdc9b8f51cb2305d57d872
fmfontana <105196681+fmfontana@users.noreply.github.com>
2022-08-11 20:43:28 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('BootstrapApp');
-  const port = process.env.PORT;
+  const port = +process.env.PORT;
 
   app.setGlobalPrefix('/api');
 
