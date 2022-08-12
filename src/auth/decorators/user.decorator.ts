6258d4cf330e705b41ee2be8729c4c97c95bc2da
fmfontana <105196681+fmfontana@users.noreply.github.com>
2022-08-12 06:30:03 -0300

    updt

---
+++ src/auth/decorators/user.decorator.ts
@@ -0,0 +1,18 @@
+import {
+  createParamDecorator,
+  ExecutionContext,
+  InternalServerErrorException,
+} from '@nestjs/common';
+
+export const GetUser = createParamDecorator(
+  (data: string, ctx: ExecutionContext) => {
+    const req = ctx.switchToHttp().getRequest();
+    const user = req.user;
+
+    if (!user) {
+      throw new InternalServerErrorException();
+    }
+
+    return !data ? user : user[data];
+  },
+);
